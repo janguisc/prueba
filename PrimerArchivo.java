f3526aa868d0f05ecefc7d83ccc798a9821ae052
janguisc <jeromianguis@gmail.com>
2024-09-26 18:51:36 +0200

    Delete PrimerArchivo.java

--- PrimerArchivo.java
+++
@@ -1,9 +0,0 @@
-public class HolaMundo {
-  public static void main (String[] args) {
-    System.out.println("Hola, mundo!");
-    System.out.println("Hola, mundo2!");
-    System.out.println("Hola, mundo3!");
-    System.out.println("hola, mundo4!");
-    System.out.println("Hola, mundo5!");	
-  }
-}
